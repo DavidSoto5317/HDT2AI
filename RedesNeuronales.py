b2d6f19098d66bf3b3dd449cfa48c54811cc6a3f
DavidSoto5317 <urielsonic@gmail.com>
2020-04-04 16:41:38 -0600

    Se sube el modulo de metodos para realizar la red neuronal y el gradiente para optimizar

--- RedesNeuronales.py
+++ RedesNeuronales.py
@@ -25,15 +25,6 @@ def inflate_matrixes(flat_thetas, shapes):
         for i in range(layers - 1)
     ]
 
-## Funcion para Paso 1 del algoritmo
-def set_delta_size(thetas):
-    arregloDeltas = []
-    for i in range(len(thetas)):
-        x, y = thetas[i].shape
-        a = np.ones(x*y).reshape(x, y)
-        arregloDeltas.append(a)
-    return arregloDeltas
-
 ## Paso 2 del algoritmo
 ## Funcion para Paso 2.1 y 2.2
 def feed_propagation(thetas, X):
@@ -58,16 +49,12 @@ def feed_propagation(thetas, X):
         )
     return listaDeMatricesA
 
-## Funcion sigmoide para el paso 2.2
+# Funcion Sigmoide
 def sigmoid(z):
-    a = []
-    aValue = 0
-    for i in np.nditer(z):
-        aValue = 1/(1 + np.exp(-i))
-        a.append(aValue)
-    a = np.asarray(a).reshape(z.shape) 
-    return a
+    a = [(1 / (1 + np.exp(-i))) for i in z]
+    return np.asarray(a).reshape(z.shape)
 
+# Funcion de costo entre la prediccion y el valor esperado
 def cost_function(flat_thetas, shapes, X, Y):
     a = feed_propagation(
         inflate_matrixes(flat_thetas, shapes),
@@ -83,111 +70,32 @@ def cost_bayesian_neural_network(flat_thetas, shapes, X, Y):
     a = feed_propagation(thetas, X)
     deltas = [*range(layers - 1), a[-1] - Y]
 
-    # Paso 2.4 (V2.0)
-    # for i in range(layers - 2, 0, -1):
-    #     delta =  (deltas[i + 1] @ thetas[i]) * np.hstack((
-    #         ## Aqui se le agrega el Bias a la matriz a^i
-    #         np.ones(len(X)).reshape(len(X), 1),
-    #         (a[i] * (1 - a[i]))
-    #     ))
-    #     deltas[i] = np.delete(delta, 0, 1)
-    # Paso 2.4 (V1.0)
+    # Paso 2.4
     for i in range(layers - 2, 0, -1):
-        deltas[i] =  (deltas[i + 1] @ np.delete((thetas[i]), 0, 1)) * (a[i] * (1 - a[i]))
+        deltas[i] =  (deltas[i + 1] @ np.delete(thetas[i], 0, 1)) * (a[i] * (1 - a[i]))
 
-    # Paso 2.5 (V2.0)
-    # arregloDeltas = []
-    # for n in range(layers - 1):
-    #     f1, c1 = thetas[n].shape
-    #     deltaMayuscula = np.zeros(f1*c1).reshape(f1,c1) 
-    #     f2, _ = a[n].shape
-    #     for k in range(f2): 
-    #         f3 = len(deltas[n + 1][k])
-    #         f4 = len(a[n][k])
-    #         deltaMayuscula = deltaMayuscula + ((deltas[n + 1][k]).reshape(f3,1) * ((np.append([1],a[n][k])).reshape(f4 + 1,1)).T)
-
-    #     arregloDeltas.append(deltaMayuscula) 
-
-    # Paso 2.5 (V1.0)
+    # Paso 2.5
     arregloDeltas = []
     for n in range(layers - 1):
-        f1, c1 = thetas[n].shape
-        deltaMayuscula = np.zeros(f1*c1).reshape(f1,c1) 
-        f2, _ = a[n].shape
-        for k in range(f2): 
-            f3 = len(deltas[n + 1][k])
-            f4 = len(a[n][k])
-            deltaMayuscula = deltaMayuscula + ((deltas[n + 1][k]).reshape(f3,1) @ ((np.append([1],a[n][k])).reshape(f4 + 1,1)).T)
-
-        arregloDeltas.append(deltaMayuscula) 
+        arregloDeltas.append(
+            (deltas[n + 1].T 
+            @ 
+            np.hstack((
+                np.ones(len(a[n])).reshape(len(a[n]), 1),
+                a[n]
+            ))) / m
+        )
 
     arregloDeltas = np.asarray(arregloDeltas)
 
     # Paso 3
-    return flatten_list_of_arrays([
-        np.true_divide(arreglo, m)
-        for arreglo in arregloDeltas
-    ])
+    return flatten_list_of_arrays(
+        arregloDeltas
+    )
 
+## Realiza la prediccion del algoritmo
+def prediction(flat_thetas, shapes, X, Y):
+    thetas = inflate_matrixes(flat_thetas, shapes)
+    a = feed_propagation(thetas, X)
 
-## Cargamos datos
-## Iniciamos a probar con datos de training de fashion
-# Se cargan los datoss  
-datos = pd.read_csv('data/train/fashion-mnist_train.csv')
-
-# Se procesa el dataset
-X = datos.iloc[:, 1:] / 1000.0 #Normalizacion de los datos
-m, n = X.shape
-y = np.asarray(datos.iloc[:, 0])
-y = y.reshape(m, 1)
-Y = (y == np.array(range(10))).astype(int)
-
-# # Se hace un set de la arquitectura de la red neuronal
-NETWORK_ARCHITECTURE = np.array([
-    n,
-    130,
-    10
-])
-
-
-theta_shapes = np.hstack((
-    NETWORK_ARCHITECTURE[1:].reshape(len(NETWORK_ARCHITECTURE) - 1, 1),
-    (NETWORK_ARCHITECTURE[:-1] + 1).reshape(len(NETWORK_ARCHITECTURE) - 1, 1)
-))
-
-flat_thetas = flatten_list_of_arrays([
-    np.random.rand(*theta_shape) 
-    for theta_shape in theta_shapes
-    ])
-
-# cost_bayesian_neural_network(flat_thetas, theta_shapes, X, Y)
-print("Optimazing...")
-result = op.minimize(
-    fun=cost_function,
-    x0=flat_thetas,
-    args=(theta_shapes, X, Y),
-    method='L-BFGS-B',
-    jac=cost_bayesian_neural_network,
-    options={'disp': True, 'maxiter': 3000}
-)
-
-print("Optimized")
-
-
-# Se imprimen los resultados
-print(result)
-print(result.x)
-
-# Se escribe el resultado en un archivo
-outfile = open("model_trained", "wb")
-pickle.dump(result.x, outfile)
-outfile.close()
-
-with (open("model_trained", "rb")) as openfile:
-    while True:
-        try:
-            thetasOptimized = pickle.load(openfile)
-        except EOFError:
-            break
-
-print(np.asarray(thetasOptimized))
+    return [a[-1] , Y]
